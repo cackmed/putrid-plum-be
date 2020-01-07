26d518205c1104d391f05f3ad70b7a62cc937892
Caleb Pendergraft <cackmed@gmail.com>
2020-01-06 17:31:52 -0800

    film model routes, and supporting trsts/other model refactoring

---
+++ __tests__/film.test.js
@@ -0,0 +1,93 @@
+require('dotenv').config();
+
+const request = require('supertest');
+const app = require('../lib/app');
+const connect = require('../lib/utils/connect');
+const mongoose = require('mongoose');
+const Film = require('../lib/models/Film');
+const Studio = require('../lib/models/Studio');
+const Actor = require('../lib/models/Actor');
+
+describe('Film routes', () => {
+  beforeAll(() => {
+    connect();
+  });
+
+  beforeEach(() => {
+    return mongoose.connection.dropDatabase();
+  });
+
+  let actor;
+  let film;
+  let studio;
+  beforeEach(async() => {
+    studio = await Studio.create({
+      name: 'Universal Studios',
+      address: [{ city: 'LA', state: 'Cal', country: 'USA', }]
+    });
+
+    actor = await Actor.create({ name: 'Al Pacino', dob: new Date('04/25/1940'), pob: 'New York City' });
+
+    film = await Film.create({
+      title: 'The Irishman',
+      studio: studio._id,
+      released: 2019,
+      cast: [{ role: 'Jimmy Hoffa', actor: actor._id }]
+    });
+  });
+
+  afterAll(() => {
+    return mongoose.connection.close();
+  });
+  it('creates a Film', () => {
+    return request(app)
+      .post('/api/v1/film')
+      .send({
+        title: 'The Irishman',
+        studio: studio._id,
+        released: 2019,
+        cast: [{ role: 'Jimmy Hoffa', actor: actor._id }]
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          title: 'The Irishman',
+          studio: studio._id.toString(),
+          released: 2019,
+          cast: [{ _id: expect.any(String), role: 'Jimmy Hoffa', actor: actor._id.toString() }],
+          __v: 0
+        });
+      });
+  });
+  it('gets all Films', async() => {
+    const films = await Film.create([
+      { title: 'what', studio: studio._id, released: Date.now(), cast: [{ role: 'Who', actor: actor._id }] },
+      { title: 'huh', studio: studio._id, released: Date.now(), cast: [{ role: 'What', actor: actor._id }]  },
+      { title: 'maybe', studio: studio._id, released: Date.now(), cast: [{ role: 'I dont give a damn', actor: actor._id }]  }
+    ]);
+
+    return request(app)
+      .get('/api/v1/film')
+      .then(res => {
+        films.forEach(film => {
+          expect(res.body).toContainEqual(JSON.parse(JSON.stringify(film)));
+        });
+      });
+  });
+  
+  it('gets an film by id', async() => {
+    return request(app)
+      .get(`/api/v1/film/${film._id}`)
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          title: expect.any(String),
+          studio:  JSON.parse(JSON.stringify(studio)),
+          released: expect.any(Number),
+          cast: [{ _id: expect.any(String), role: expect.any(String), actor: actor._id.toString() }],
+          __v: 0
+        });
+      });
+  });
+  
+});
